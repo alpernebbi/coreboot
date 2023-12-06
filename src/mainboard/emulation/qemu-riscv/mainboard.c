21d9e107b3a1cf646a7b9122b1ae9594ccf03b27
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2023-12-07 01:06:51 +0300

    mainboard/qemu-riscv: Add PCI support
    Add PCI support for the qemu-riscv mainboard like how it is done for
    qemu-aarch64. The resulting build can be used with a QEMU command line
    like:
    
        qemu-system-riscv64 \
            -display gtk,show-tabs=on \
            -bios build/coreboot.rom \
    	-drive if=pflash,file=build/coreboot.rom,format=raw \
            -M virt -m 1G \
            -device pcie-root-port \
            -serial stdio -device bochs-display \
            ...
    
    One complication is that the PCIe high MMIO base depends on the amount
    of available RAM for 64-bit RISC-V VMs [1], but not for 32-bit ones.
    
    [1] https://gitlab.com/qemu-project/qemu/-/blob/v8.2.2/hw/riscv/virt.c#L1474-1489
    
    Change-Id: Ie57b35620af82c681d1f0d78fa8e514e4df0d2ac
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- src/mainboard/emulation/qemu-riscv/mainboard.c
+++ src/mainboard/emulation/qemu-riscv/mainboard.c
@@ -4,6 +4,7 @@
 #include <device/device.h>
 #include <symbols.h>
 #include <cbmem.h>
+#include <mainboard/addressmap.h>
 
 static void mainboard_enable(struct device *dev)
 {
@@ -16,4 +17,39 @@ static void mainboard_enable(struct device *dev)
 
 struct chip_operations mainboard_ops = {
 	.enable_dev = mainboard_enable,
+};
+
+static void qemu_riscv_domain_read_resources(struct device *dev)
+{
+	struct resource *res;
+	int index = 0;
+	/* Initialize the system-wide I/O space constraints. */
+	res = new_resource(dev, index++);
+	res->limit = 0xffff;
+	res->flags = IORESOURCE_IO | IORESOURCE_ASSIGNED;
+
+	/* Initialize the system-wide memory resources constraints. */
+	res = new_resource(dev, index++);
+	res->base = QEMU_VIRT_PCIE_LOW_MMIO_BASE;
+	res->limit = res->base + QEMU_VIRT_PCIE_LOW_MMIO_SIZE - 1;
+	res->flags = IORESOURCE_MEM | IORESOURCE_ASSIGNED;
+
+	res = new_resource(dev, index++);
+	if (CONFIG(ARCH_RISCV_RV32)) {
+		res->base = QEMU_VIRT32_PCIE_HIGH_MMIO_BASE;
+		res->limit = res->base + QEMU_VIRT32_PCIE_HIGH_MMIO_SIZE - 1;
+	} else {
+		res->base = ALIGN_UP((uintptr_t)_dram + (uintptr_t)cbmem_top(),
+				     QEMU_VIRT64_PCIE_HIGH_MMIO_SIZE);
+		res->limit = res->base + QEMU_VIRT64_PCIE_HIGH_MMIO_SIZE - 1;
+	}
+	res->flags = IORESOURCE_MEM | IORESOURCE_ASSIGNED;
+
+	mmconf_resource(dev, index++);
+}
+
+struct device_operations qemu_riscv_pci_domain_ops = {
+	.read_resources    = qemu_riscv_domain_read_resources,
+	.set_resources     = pci_domain_set_resources,
+	.scan_bus          = pci_host_bridge_scan_bus,
 };
