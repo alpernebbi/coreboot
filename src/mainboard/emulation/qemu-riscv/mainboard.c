be3f3598ee0561e39349bbaa9b650362f6c4af18
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2023-12-07 01:06:51 +0300

    mainboard/qemu-riscv: Add PCI support
    Add PCI support for the qemu-riscv mainboard like how it is done for
    qemu-aarch64. The resulting build can be used with a QEMU command line
    like:
    
        util/riscv/make-spike-elf.sh build/coreboot.{rom,elf}
        qemu-system-riscv64 \
            -display gtk,show-tabs=on \
            -bios none \
            -kernel build/coreboot.elf \
            -M virt -m 1G \
            -device pcie-root-port \
            -serial stdio -device VGA \
            ...
    
    One current problem is that probe_ramsize() causes an error, which has
    to be worked around by e.g. setting DRAM_SIZE_MB=1024 and adding a
    `return 1024;` to probe_ramsize() in order for the mainboard to work.
    But this is the case even before this commit.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- src/mainboard/emulation/qemu-riscv/mainboard.c
+++ src/mainboard/emulation/qemu-riscv/mainboard.c
@@ -1,22 +1,49 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <cbmem.h>
 #include <console/console.h>
 #include <device/device.h>
 #include <symbols.h>
 #include <ramdetect.h>
+#include <mainboard/addressmap.h>
 
 static void mainboard_enable(struct device *dev)
 {
-	size_t dram_mb_detected;
-
-	if (!dev) {
-		die("No dev0; die\n");
-	}
-
-	dram_mb_detected = probe_ramsize((uintptr_t)_dram, CONFIG_DRAM_SIZE_MB);
-	ram_range(dev, 0, (uintptr_t)_dram, dram_mb_detected * MiB);
 }
 
 struct chip_operations mainboard_ops = {
 	.enable_dev = mainboard_enable,
+};
+
+struct chip_operations mainboard_emulation_qemu_riscv_ops = { };
+
+static void qemu_riscv_domain_read_resources(struct device *dev)
+{
+	struct resource *res;
+	int index = 0;
+	/* Initialize the system-wide I/O space constraints. */
+	res = new_resource(dev, index++);
+	res->limit = 0xffff;
+	res->flags = IORESOURCE_IO | IORESOURCE_ASSIGNED;
+
+	/* Initialize the system-wide memory resources constraints. */
+	res = new_resource(dev, index++);
+	res->base = QEMU_VIRT_PCIE_LOW_MMIO_BASE;
+	res->limit = QEMU_VIRT_PCIE_LOW_MMIO_LIMIT;
+	res->flags = IORESOURCE_MEM | IORESOURCE_ASSIGNED;
+
+	res = new_resource(dev, index++);
+	res->base = QEMU_VIRT_PCIE_HIGH_MMIO_BASE;
+	res->limit = QEMU_VIRT_PCIE_HIGH_MMIO_LIMIT;
+	res->flags = IORESOURCE_MEM | IORESOURCE_ASSIGNED;
+
+	mmio_range(dev, index++, QEMU_VIRT_PCIE_ECAM_BASE, QEMU_VIRT_PCIE_ECAM_SIZE);
+
+	ram_from_to(dev, index++, (uintptr_t)_dram, (uintptr_t)cbmem_top());
+}
+
+struct device_operations qemu_riscv_pci_domain_ops = {
+	.read_resources    = qemu_riscv_domain_read_resources,
+	.set_resources     = pci_domain_set_resources,
+	.scan_bus          = pci_host_bridge_scan_bus,
 };
