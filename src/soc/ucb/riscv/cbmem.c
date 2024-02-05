ac9ee0faece4abf7324d7fe172149949a2edce85
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-02-05 20:16:26 +0300

    mainboard/qemu-riscv: Get top of memory from device-tree blob
    Trying to probe RAM space to figure out top of memory causes an error on
    RISC-V virtual machines with recent versions of QEMU. A previous commit
    adds a library function to parse device-tree blob that QEMU provides us.
    Use it to determine top of memory in RISC-V QEMU virtual machines, but
    still fall back to the RAM probing approach as a last-ditch effort.
    
    Change-Id: I9e4a95f49ad373675939329eef40d7423a4132ab
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- src/soc/ucb/riscv/cbmem.c
+++ src/soc/ucb/riscv/cbmem.c
@@ -3,8 +3,16 @@
 #include <cbmem.h>
 #include <symbols.h>
 #include <ramdetect.h>
+#include <device_tree.h>
+#include <mcall.h>
 
 uintptr_t cbmem_top_chipset(void)
 {
+	uintptr_t top;
+
+	top = fdt_get_memory_top((void *)HLS()->fdt);
+	if (top)
+		return top;
+
 	return (uintptr_t)_dram + (probe_ramsize((uintptr_t)_dram, CONFIG_DRAM_SIZE_MB) * MiB);
 }
