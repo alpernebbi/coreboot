fd9557c3cef6adcd8ec54e943d35f3107277b50e
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-02-05 20:22:12 +0300

    mainboard/qemu-aarch64: Get top of memory from device-tree blob
    Trying to probe RAM space to figure out top of memory causes an
    exception on AArch64 virtual machines with recent versions of QEMU, but
    we temporarily enable exception handlers for that and use it to help
    detect if a RAM address is usable or not. However, QEMU docs recommend
    reading device information from the device-tree blob it provides us at
    the start of RAM.
    
    A previous commit adds a library function to parse device-tree blob that
    QEMU provides us. Use it to determine top of memory in AArch64 QEMU
    virtual machines, but still fall back to the RAM probing approach as a
    last-ditch effort.
    
    Change-Id: I4cc888b57cf98e0797ce7f9ddfa2eb34d14cd9c1
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- src/mainboard/emulation/qemu-aarch64/cbmem.c
+++ src/mainboard/emulation/qemu-aarch64/cbmem.c
@@ -3,8 +3,15 @@
 #include <cbmem.h>
 #include <ramdetect.h>
 #include <symbols.h>
+#include <device_tree.h>
 
 uintptr_t cbmem_top_chipset(void)
 {
+	if (CONFIG(FLATTENED_DEVICE_TREE)) {
+		uint64_t top = fdt_get_memory_top((void *)_dram);
+		if (top)
+			return MIN(top, (uint64_t)4 * GiB) - 1;
+	}
+
 	return (uintptr_t)_dram + (probe_ramsize((uintptr_t)_dram, CONFIG_DRAM_SIZE_MB) * MiB);
 }
