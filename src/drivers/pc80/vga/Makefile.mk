d79ff8e0e92f7806827e5e90267c29a5cd8115d6
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-04-24 22:12:50 +0300

    drivers/pc80/vga: Allow building for non-x86 architectures
    The Cirrus driver uses the VGA support code for its initialization,
    which in turn depends on port I/O functions to talk to the PCI device.
    Previous commits implement these and fix build errors with VGA code on
    more architectures, so it's possible to have things also work there to
    an extent.
    
    However, building VGA support code is restricted to x86 architectures in
    its Makefile, so trying to build the Cirrus driver for e.g. ARM64
    results in linker errors. Remove the Makefile restriction.
    
    Change-Id: Ic3199fda500c51aa3261cd9eff9a5b0b052fd742
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- src/drivers/pc80/vga/Makefile.mk
+++ src/drivers/pc80/vga/Makefile.mk
@@ -1,7 +1,5 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
-ifeq ($(CONFIG_ARCH_X86),y)
-
 romstage-$(CONFIG_ROMSTAGE_VGA) += vga_io.c
 romstage-$(CONFIG_ROMSTAGE_VGA) += vga_palette.c
 romstage-$(CONFIG_ROMSTAGE_VGA) += vga_font_8x16.c
@@ -11,5 +9,3 @@ ramstage-$(CONFIG_VGA) += vga_io.c
 ramstage-$(CONFIG_VGA) += vga_palette.c
 ramstage-$(CONFIG_VGA) += vga_font_8x16.c
 ramstage-$(CONFIG_VGA) += vga.c
-
-endif
